e651e799c812fc4bd36f7b3a53d53ebc7d0603bb
ankithbolem <100229084+ankithbolem@users.noreply.github.com>
2022-08-23 18:48:34 +0530

    Create tomcat.groovy

---
+++ vars/tomcat.groovy
@@ -0,0 +1,11 @@
+def call(ip,user)
+{
+                  sshagent(['dada']) {
+             // copy war file
+                    sh "scp -o StrictHostKeyChecking=no target/*.war ${user}@${ip}:/opt/tomcat9/webapps"
+             // shutdown server
+             sh "ssh -o StrictHostKeyChecking=no ${user}@${ip} /opt/tomcat9/bin/shutdown.sh"
+             // start server
+             sh "ssh ${user}@${ip} /opt/tomcat9/bin/startup.sh"
+
+                  }
